8c3d77ac72c765cc54e1ea7c21b71cc57d25e417
Willian Veloso <willianveloso@alumni.usp.br>
2024-02-24 18:02:27 -0300

    Updated args and executable path for Puppeteer Launch

--- scrapeLogic.js
+++ scrapeLogic.js
@@ -1,8 +1,20 @@
 const puppeteer = require("puppeteer");
+require("dotenv").config();
 
 const scrapeLogic = async (res) => {
     // Launch the browser and open a new blank page
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({
+    args: [
+        "--disable-setupid-sandbox",
+        "--no-sandbox",
+        "--single-process",
+        "--no-zygote",
+    ],
+    executablePath:
+    process.env.NODE_ENV === "production"
+     ? process.env.PUPPETEER_EXECUTABLE_PATH
+     : puppeteer.executablePath(),
+  });
   try{
     const page = await browser.newPage();
 
