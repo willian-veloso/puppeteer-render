652e9fa676a2e9c628b7aca05d5667e11087bdf1
Willian Veloso <willianveloso@alumni.usp.br>
2024-02-24 18:36:51 -0300

    Updated scrapeLogic

--- scrapeLogic.js
+++ scrapeLogic.js
@@ -1,9 +1,51 @@
-const puppeteer = require("puppeteer");
+const pup = require("puppeteer");
 require("dotenv").config();
+
+const url = "https://drcalc.net/consultaindices.asp?ml=Series&categoria=1&it=1"
+
+const selectSelector = ['select[name="imes"]','select[name="iano"]', 'select[name="codigo"]']
+const optionValue = ['01','1930'];
+const cellSelector = 'td.tabindmenu2';
+
+async function selecionarOpcao(page, selectSelector, optionValue) {
+    await page.evaluate((selectSelector, optionValue) => {
+      const select = document.querySelector(selectSelector);
+      const option = [...select.options].find(opt => opt.value === optionValue);
+
+      if (option) {
+        option.selected = true;
+        const event = new Event('change', { bubbles: true });
+        select.dispatchEvent(event);
+      } else {
+        console.error('Opção não encontrada:', optionValue);
+      }
+    }, selectSelector, optionValue);
+  }
+
+async function obterDadosDaTabela(page, cellSelector) {
+return page.$$eval(cellSelector, (cells) => {
+    if (cells.length === 0) {
+    console.error('Nenhuma célula encontrada');
+    return null;
+    }
+
+    const tabela = cells[0].closest('table');
+    const linhas = tabela.querySelectorAll('tr');
+
+    const dados = [];
+    linhas.forEach((linha) => {
+    const celulas = linha.querySelectorAll('td');
+    const linhaData = Array.from(celulas).map(celula => celula.textContent.trim());
+    dados.push(linhaData);
+    });
+
+    return dados;
+});
+}
 
 const scrapeLogic = async (res) => {
     // Launch the browser and open a new blank page
-  const browser = await puppeteer.launch({
+  const browser = await pup.launch({
     args: [
         "--disable-setupid-sandbox",
         "--no-sandbox",
@@ -18,38 +60,32 @@ const scrapeLogic = async (res) => {
   try{
     const page = await browser.newPage();
 
-    // Navigate the page to a URL
-    await page.goto('https://developer.chrome.com/');
+    await page.goto(url);
+    
+    await page.waitForSelector('select[name="imes"]');
 
-    // Set screen size
-    await page.setViewport({width: 1080, height: 1024});
+    await selecionarOpcao(page, selectSelector[0], optionValue[0]);
+    await selecionarOpcao(page, selectSelector[1], optionValue[1]);
+    await page.select(selectSelector[2], '33')
 
-    // Type into search box
-    await page.type('.devsite-search-field', 'automate beyond recorder');
+    await Promise.all([
+        page.waitForNavigation(),
+        page.click('input[name="submit1"]')
+    ]);
 
-    // Wait and click on first result
-    const searchResultSelector = '.devsite-result-item-link';
-    await page.waitForSelector(searchResultSelector);
-    await page.click(searchResultSelector);
+    await page.waitForSelector('td.tabindmenu2');
 
-    // Locate the full title with a unique string
-    const textSelector = await page.waitForSelector(
-        'text/Customize and automate'
-    );
-    const fullTitle = await textSelector?.evaluate(el => el.textContent);
+    const tableData = await obterDadosDaTabela(page, cellSelector);
 
-    // Print the full title
-    const logStatement = `The title of this blog post is ${fullTitle}`
-    console.log(logStatement);
-    res.send(logStatement);
-    } catch (e) {
+    console.log(tableData);
+    res.send(tableData);
+    
+} catch (e) {
         console.error(e);
         res.send(`Something went wrong while running Puppeteer: ${e}`)
     } finally {
         await browser.close();
     }
-
-  
 };
 
 module.exports= { scrapeLogic };
